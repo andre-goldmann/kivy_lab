60e6a7a4e026ad65c6b4e3dbbddea5783ac7dfa2
goldmann <andre.goldmann@saxess.ag>
2022-01-30 17:25:54 +0100

    Layouts

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 
 class BoxLayoutExample(BoxLayout):
   pass
